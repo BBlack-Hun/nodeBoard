c54319c294aae754b47bf8aa0541b5dab71d7a00
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-27 23:06:00 +0900

    module

--- servers.js
+++ servers.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('bodyparser');
+const path = require('path');
+
 const app = express();
 
 // path를 지정하여 해당 파일을 가리켜준다.
@@ -9,6 +12,19 @@ const PORT = process.env.PORT || 8080;
 
 // log requests
 app.use(morgan('tiny'));
+
+// parse request to body-psrser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine (사용할 형식을 기입 : ejs)
+app.set('view engine', 'ejs');
+// 위와 동일한 의미
+//app.set('views', path.resolve(__dirname, 'views/ejs'));
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/', (req, res) => {
   res.send('Curd Application');

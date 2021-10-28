80aaf590b5a1f4406bb90e3811f6a83984f95f87
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-29 00:59:00 +0900

    route

--- servers.js
+++ servers.js
@@ -21,25 +21,13 @@ app.set('view engine', 'ejs');
 // 위와 동일한 의미
 //app.set('views', path.resolve(__dirname, 'views/ejs'));
 
+// load routers
+app.use('/', require('./server/routes/router'));
+
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
 app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
 app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
-
-app.get('/', (req, res) => {
-  //   res.send('Curd Application');
-  res.render('index');
-});
-
-app.get('/add_user', (req, res) => {
-  //   res.send('Curd Application');
-  res.render('add_user');
-});
-
-app.get('/update_user', (req, res) => {
-  //   res.send('Curd Application');
-  res.render('update_user');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
